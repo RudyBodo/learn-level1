cb2d551f0f386a6b85b6d5ba1ad527543984a61b
rudybodo <transkmuba@gmail.com>
2015-06-09 01:17:13 +0800

    operasi matematika

--- php/operators/operator-aritmatika.php
+++ php/operators/operator-aritmatika.php
@@ -4,18 +4,18 @@ $angka1 = (int) 40;
 $angka2 = 2;
 
 // pertambahan
-echo $angka1 + $angka2;
+echo $angka1 + $angka2 ."\n";
 
 // pengurangan
-echo $angka1 - $angka2;
+echo $angka1 - $angka2 . "\n";
 
 // perkalian
-echo $angka1 * $angka2;
+echo $angka1 * $angka2 ."\n";
 
 // pembagi
-echo $angka1 / $angka2;
+echo $angka1 / $angka2 ."\n";
 
 // operator aritmatika dan assignment (memberikan nilai ke variabel lain)
-$hasil = $angka1 + $angka2;
+$hasil = $angka1 + $angka2. "\n";
 
 ?>
