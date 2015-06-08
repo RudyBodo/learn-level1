80b27347893b7ec8df80b3172eecd67cb92e7564
rudybodo <transkmuba@gmail.com>
2015-06-09 00:46:42 +0800

    operator logika

---
+++ php/operators/operator-logic.php
@@ -0,0 +1,26 @@
+<?php
+
+//operator logika
+$hasil1 = true and false;
+echo '$hasil1 = ';
+echo var_dump($hasil1)."\n"; // $hasil1 = bool(true) 
+ 
+$hasil2 = (true and false);
+echo '$hasil2 = ';
+echo var_dump($hasil2)."\n"; // $hasil2 = bool(false) 
+ 
+$hasil3 = (true xor false);
+echo '$hasil3 = ';
+echo var_dump($hasil3)."\n"; // $hasil3 = bool(true) 
+ 
+$hasil4 = (false or true && false);
+echo '$hasil4 = ';
+echo var_dump($hasil4)."\n"; // $hasil4 = bool(false) 
+ 
+$a=true;
+$b=false;
+$hasil5 = ($a and $b || $a or b);
+echo '$hasil5 = ';
+echo var_dump($hasil5); // $hasil5 = bool(true)
+
+?>
