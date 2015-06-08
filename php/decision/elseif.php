b18ec91c1a2b79aabc582c882b25a558ecfb7720
rudybodo <transkmuba@gmail.com>
2015-06-09 03:08:30 +0800

    pengamblilan keputusan metode switch

---
+++ php/decision/elseif.php
@@ -0,0 +1,13 @@
+<?php
+
+$a=15;
+$b=15;
+ 
+	if ($a > $b):
+	    echo "a lebih besar daripada b \n";
+	elseif ($a == $b):
+	    echo "a sama besar dengan b \n";
+	else:
+	    echo "a lebih kecil daripada b \n";
+	endif;
+?>
