3b47f9b5dcfcdabb8c379efd4f45f8ce8ebd5e0f
rudybodo <transkmuba@gmail.com>
2015-06-20 00:32:42 +0800

    array associative

---
+++ php/array/associative_array.php
@@ -0,0 +1,19 @@
+ <?php
+ 
+	$pacarku = array (
+	array ( nama=>"Rosa",
+	sifat=>"Baik Hati",
+	umur=>17 ),
+	array ( nama=>"Ruby",
+	sifat=>"Tidak Sombong",
+	umur=>27 ),
+	array ( nama=>"Sri",
+	sifat=>"Rajin Menabung",
+	umur=>37 ),
+	array ( nama=>"Nety",
+	sifat=>"Pemalas",
+	umur=>15 ));
+	 
+	print_r($pacarku);
+ 
+?>
